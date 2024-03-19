3b94d611fe1bf60ddc0655c583cf5b50a84b2c92
Nwafor Ugochukwu Emmanuel <nwaforugochukwu21@gmail.com>
2024-03-19 13:03:17 +0000

    Hot fix

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """
-an asychronous coroutine that takes in an integer argument(max_delay, with a default of 10) named wait_random that waits for a random delay between 0 and max_delay (included and float value) seconds and eventually returns it.
+an asychronous coroutine that takes in an integer argument(max_delay, with a
+default of 10) named wait_random that waits for a random delay between 0 and
+max_delay (included and float value) seconds and eventually returns it.
 """
 
 
 import asyncio
 import random
-
 
 
 async def wait_random(max_delay: int = 10) -> float:
